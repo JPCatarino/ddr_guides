1d8896d71ac6768a912ea2fe89100cca48bc3dcc
JPCatarino <jorge.catarino@ua.pt>
2021-04-07 18:41:44 +0100

    [GUIDE1] Fixing 4.c to actually respect enunciado and calculate prob of having an normal connection when there's errors

--- guide1/task4.m
+++ guide1/task4.m
@@ -28,22 +28,24 @@ res_5 = res_4*transitions(4);
 X = [res_1 res_2 res_3 res_4 res_5];
 
 prob_normal = X(1) + X(2) + X(3);
-prob_interference = X(4) + X(5);
+prob_interf = X(4) + X(5);
 
 disp(' ');
 disp("Ex4.a.");
 disp("Probability of the link being in normal state = " + prob_normal);
-disp("Probability of the link being in interference state = " + prob_interference);
+disp("Probability of the link being in interference state = " + prob_interf);
 
 %4b
 disp(' ');
 disp("Ex4.b.");
 average_normal = ((X(1) * 10^-6) + (X(2) * 10^-5) + (X(3) * 10^-4))/prob_normal; 
-average_interference = ((X(4)*10^-3) + (X(5)*10^-2))/prob_interference;
+average_interference = ((X(4)*10^-3) + (X(5)*10^-2))/prob_interf;
 disp("Average ber of the link in normal state = " + average_normal);
 disp("Average ber of the link in interference state = " + average_interference);
 
 %4c
+% Ver slides 1-7
+
 disp(' ');
 disp("Ex4.c.");
 % Packet size from 64 bytes to 200 bytes
@@ -51,18 +53,32 @@ n = linspace(64, 200);
 
 %With Errors
 i = 0;
-p_normal = [(10^-6), (10^-5), (10^-4)];
+ber_normal = [(10^-6), (10^-5), (10^-4)];
+ber_interf = [(10^-3), (10^-2)];
 
-f1 = (1 - (p_normal(1)^i * (1 - p_normal(1)).^(n))) * 100;
-f2 = (1 - (p_normal(2)^i * (1 - p_normal(2)).^(n))) * 100;
-f3 = (1 - (p_normal(3)^i * (1 - p_normal(3)).^(n))) * 100;
+f1 = (1 - (ber_normal(1)^i * (1 - ber_normal(1)).^(n))) * 100;
+f2 = (1 - (ber_normal(2)^i * (1 - ber_normal(2)).^(n))) * 100;
+f3 = (1 - (ber_normal(3)^i * (1 - ber_normal(3)).^(n))) * 100;
+f4 = (1 - (ber_interf(1)^i * (1 - ber_interf(1)).^(n))) * 100;
+f5 = (1 - (ber_interf(2)^i * (1 - ber_interf(2)).^(n))) * 100;
+
+prob_errors_in_normal = f1 + f2 + f3; 
+prob_errors_in_interf = f4 + f5;
+
+% P(Errors) = P(Errors|N)P(N) + P(Errors|I)P(I)
+prob_errors = (prob_errors_in_normal .* prob_normal) + ... 
+              (prob_errors_in_interf .* prob_interf);
+
+% P(CondN|Errors) = P(Errors|N)P(N)/P(Errors)
+prob_normal_with_errors = (prob_errors_in_normal .* prob_normal)./prob_errors; 
 
 figure(1);
-semilogy(n, f1, 'b-', n, f2, 'b--', n, f3, 'b:');
+semilogy(n, prob_normal_with_errors, 'b-');
 grid on
-title("Probabilidade de receber o pacote com erros(%)")
+title("Probabilidade de link estar em estado normal se o pacote for recebido com erros(%)")
 xlabel('Packet Size (Bytes)')
-legend('ber=1e-6', 'ber=1e-5', 'ber=1e-4', 'location', 'southwest')
+legend('%', 'location', 'southwest')
+
 
 
 
