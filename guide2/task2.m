a32241cca3e97271fb03009b9fad8adfb7fdfd8c
JPCatarino <jorge.catarino@ua.pt>
2021-04-27 12:10:13 +0100

    [GUIDE2] Add task2 b, c and d

--- guide2/task2.m
+++ guide2/task2.m
@@ -24,7 +24,7 @@ W = 0;
 p = 0.2;
 R = 10000;
 
-[medias_b_4k, terms_b_4k, medias_b_hd, terms_b_hd] = ...
+[medias_b_4k_c1, terms_b_4k_c1, medias_b_hd_c1, terms_b_hd_c1] = ...
     runSimulator2(N_times, alfa, lambda_values, p, c1_n, c1_S, W, R, fname);
 
 % 4k Blocking Probability
@@ -32,14 +32,14 @@ figure(1)
 tiledlayout(1,2)
 
 nexttile;
-bar(lambda_values,medias_b_4k) 
+bar(lambda_values,medias_b_4k_c1) 
 title("Blocking Probability 4K (%)")
 xlabel('\lambda (request/hour)')
 ylim([0 100])
 
 hold on
 
-er = errorbar(lambda_values, medias_b_4k, terms_b_4k);    
+er = errorbar(lambda_values, medias_b_4k_c1, terms_b_4k_c1);    
 er.Color = [0 0 0];                            
 er.LineStyle = 'none';  
 
@@ -47,15 +47,120 @@ hold off
 
 % HD Blocking Probability
 nexttile;
-bar(lambda_values,medias_b_hd)    
+bar(lambda_values,medias_b_hd_c1)    
 title("Blocking Probability HD (%)")
 xlabel('\lambda (request/hour)')
 ylim([0 100])
 
 hold on
 
-er = errorbar(lambda_values, medias_b_hd, terms_b_hd);    
+er = errorbar(lambda_values, medias_b_hd_c1, terms_b_hd_c1);    
 er.Color = [0 0 0];                            
 er.LineStyle = 'none';  
 
 hold off
+
+%%
+%2b
+
+N_times = 10;
+alfa = 0.1;
+W = 0;
+p = 0.2;
+R = 10000;
+
+[medias_b_4k_c2, terms_b_4k_c2, medias_b_hd_c2, terms_b_hd_c2] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c2_n, c2_S, W, R, fname);
+
+[medias_b_4k_c3, terms_b_4k_c3, medias_b_hd_c3, terms_b_hd_c3] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c3_n, c3_S, W, R, fname);
+
+figure(2);
+
+tiledlayout(1,2)
+
+nexttile;
+bar(lambda_values, [medias_b_4k_c1(:) medias_b_4k_c2(:) medias_b_4k_c3(:)]) 
+legend("Configuration 1", "Configuration 2", "Configuration 3", "Location" ,"northwest")
+title("Blocking Probability 4K (%)")
+xlabel('\lambda (request/hour)')
+ylim([0 100])
+
+nexttile;
+bar(lambda_values, [medias_b_hd_c1(:) medias_b_hd_c2(:) medias_b_hd_c3(:)]) 
+legend("Configuration 1", "Configuration 2", "Configuration 3", "Location" ,"northwest")
+title("Blocking Probability HD (%)")
+xlabel('\lambda (request/hour)')
+ylim([0 100])
+
+%%
+%2c
+
+N_times = 10;
+alfa = 0.1;
+W = 400;
+p = 0.2;
+R = 10000;
+
+[medias_b_4k_c1, terms_b_4k_c1, medias_b_hd_c1, terms_b_hd_c1] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c1_n, c1_S, W, R, fname);
+
+[medias_b_4k_c2, terms_b_4k_c2, medias_b_hd_c2, terms_b_hd_c2] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c2_n, c2_S, W, R, fname);
+
+[medias_b_4k_c3, terms_b_4k_c3, medias_b_hd_c3, terms_b_hd_c3] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c3_n, c3_S, W, R, fname);
+
+figure(3);
+
+tiledlayout(1,2)
+
+nexttile;
+bar(lambda_values, [medias_b_4k_c1(:) medias_b_4k_c2(:) medias_b_4k_c3(:)]) 
+legend("Configuration 1", "Configuration 2", "Configuration 3", "Location" ,"northwest")
+title("Blocking Probability 4K (%)")
+xlabel('\lambda (request/hour)')
+ylim([0 100])
+
+nexttile;
+bar(lambda_values, [medias_b_hd_c1(:) medias_b_hd_c2(:) medias_b_hd_c3(:)]) 
+legend("Configuration 1", "Configuration 2", "Configuration 3", "Location" ,"northwest")
+title("Blocking Probability HD (%)")
+xlabel('\lambda (request/hour)')
+ylim([0 100])
+
+%%
+%2d
+
+N_times = 10;
+alfa = 0.1;
+W = 600;
+p = 0.2;
+R = 10000;
+
+[medias_b_4k_c1, terms_b_4k_c1, medias_b_hd_c1, terms_b_hd_c1] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c1_n, c1_S, W, R, fname);
+
+[medias_b_4k_c2, terms_b_4k_c2, medias_b_hd_c2, terms_b_hd_c2] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c2_n, c2_S, W, R, fname);
+
+[medias_b_4k_c3, terms_b_4k_c3, medias_b_hd_c3, terms_b_hd_c3] = ...
+    runSimulator2(N_times, alfa, lambda_values, p, c3_n, c3_S, W, R, fname);
+
+figure(4);
+
+tiledlayout(1,2)
+
+nexttile;
+bar(lambda_values, [medias_b_4k_c1(:) medias_b_4k_c2(:) medias_b_4k_c3(:)]) 
+legend("Configuration 1", "Configuration 2", "Configuration 3", "Location" ,"northwest")
+title("Blocking Probability 4K (%)")
+xlabel('\lambda (request/hour)')
+ylim([0 100])
+
+nexttile;
+bar(lambda_values, [medias_b_hd_c1(:) medias_b_hd_c2(:) medias_b_hd_c3(:)]) 
+legend("Configuration 1", "Configuration 2", "Configuration 3", "Location" ,"northwest")
+title("Blocking Probability HD (%)")
+xlabel('\lambda (request/hour)')
+ylim([0 100])
