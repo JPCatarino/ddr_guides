c06a753e5b9da3271d2018a9de67b02624fb0615
JPCatarino <jorge.catarino@ua.pt>
2021-06-01 13:01:29 +0100

    [GUIDE3] Add 1c

--- guide3/Task2.m
+++ guide3/Task2.m
@@ -40,14 +40,31 @@ fprintf('Throughput (Mbps) = %.2e +- %.2e\n', medias_TT, temp_TT);
 
 %% 2c)
 % Parameters
-P = 10000; %stoping criterion
-alpha = 0.10; %confidence intervals
 lambda = 1800; %packet rate
 C = 10; %conection capacity
 f = 1000000; %queue size
 b = 10^-6; %bit error rate
 
+% sum(pi * (1 - Pi))
+Pi_64 = (1 - b)^(8*64);
+Pi_110 = (1 - b)^(8*110);
+Pi_1518 = (1 - b)^(8*1518);
+PacketLoss = (0.16*(1 - Pi_64)) + (0.25 * (1 - Pi_110)) ...
+    + (0.20 * (1 - Pi_1518)); 
 
+aux= [65:109 111:1517];
+PacketLoss_aux = 0;
+for i=1:length(aux)
+    Pi_aux = (1 - b)^(8*aux(i));
+    PacketLoss_aux = PacketLoss_aux + (0.39*(1 - Pi_aux));
+end
+
+PacketLoss_aux = PacketLoss_aux / length(aux);
+
+PacketLoss = PacketLoss + PacketLoss_aux;
+
+fprintf('\n2c\n');
+fprintf('PacketLoss (%%) = %.5f \n', PacketLoss*100);
 
 
 
